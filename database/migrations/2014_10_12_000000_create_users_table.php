e2eb4fb7de9950de24333b9c7fac2aa479170b2a
Albert Gaya <albertgaya@ymail.com>
2021-08-22 23:10:02 +0800

    Modify users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_name')->unique();
+            $table->text('avatar');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->dateTime('registered_at');
             $table->rememberToken();
             $table->timestamps();
         });
